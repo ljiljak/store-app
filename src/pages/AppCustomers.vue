d6c74c6df113a667344e483815a87b3ae8e254bb
Aleksandar Ljiljak <aleksandarljiljak@AleksanrMacBook.lan>
2018-05-01 17:00:49 +0200

    Customers service

--- src/pages/AppCustomers.vue
+++ src/pages/AppCustomers.vue
@@ -1,12 +1,14 @@
-template>
-  <div>
-      Customers
-  </div>
-
+<template>
+    <div>
+        <CustomersList />
+    </div>
 </template>
 
 <script>
+    import CustomersList from '../components/CustomersList.vue'
     export default {
-
+        components: {
+            CustomersList
+        }
     }
 </script>
